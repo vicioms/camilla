5b66739ccd5d1017c1f877bbd36e608c33454dd7
vicioms <vincenzomaria.schimmenti@gmail.com>
2025-12-01 00:28:38 +0100

    Theoretically working now

--- earthquakes/sequences.py
+++ earthquakes/sequences.py
@@ -3,40 +3,45 @@ import pandas as pd
 import torch
 from typing import List, Union
 
-def extract_subsequences(arrival_times: np.ndarray, features: np.ndarray, max_num_sequences : int, duration_scale : float, random_seed : Union[int, None] = None):
-    if( random_seed is not None ):
-        np.random.seed(random_seed)
-    t_min = arrival_times.min()
-    t_max = arrival_times.max()
-    t_start = np.random.uniform(t_min, t_max, size=max_num_sequences)
-    durations = np.random.exponential(scale=duration_scale, size=max_num_sequences)
-    t_end = np.minimum(t_start + durations, t_max)
-    masks = arrival_times[None, :] >= t_start[:, None]
-    masks = masks & (arrival_times[None, :] <= t_end[:, None])
-    valid_masks = masks.sum(axis=1) > 0
-    masks = masks[valid_masks, :]
-    t_start = t_start[valid_masks]
-    t_end = t_end[valid_masks]
-    subsequence_arrival_times = [ arrival_times[masks[index, :]] for index in range(masks.shape[0]) ]
-    subsequence_features = [ features[masks[index, :], :] for index in range(masks.shape[0]) ]
-    return subsequence_arrival_times, subsequence_features, t_start, t_end
-def pack_sequences(feature_list : List[np.ndarray], batch_first : bool, valid_mask_is_true : bool, sequence_has_inter_times : bool):
-    num_of_events = np.array([len(features) for features in feature_list])
-    max_num_events = num_of_events.max()
-    num_sequences = len(feature_list)
-    # we first construct batch first version
-    packed_features = np.zeros((num_sequences, max_num_events, feature_list[0].shape[1]), dtype=np.float32)
-    masks = np.full((num_sequences, max_num_events), fill_value=not valid_mask_is_true, dtype=bool)
-    for index in range(num_sequences):
-        n_events = num_of_events[index]
-        if sequence_has_inter_times:
-            n_events -= 1
-        packed_features[index, 0:n_events, :] = feature_list[index]
-        masks[index, 0:n_events] = valid_mask_is_true
-    if(not batch_first):
-        packed_features = np.transpose(packed_features, (1,0,2))
-    return packed_features, num_of_events, masks
+class Sequence:
+    def __init__(self, arrival_times: np.ndarray, features: np.ndarray):
+        self.arrival_times = arrival_times
+        self.features = features
 
-def arrival_to_inter_times(arrival_times : np.ndarray, t_start : float, t_end : float):
-    inter_times = np.diff(arrival_times, prepend=t_start, append=t_end)
-    return inter_times
+    def sample_sequences(self, max_num_sequences : int, 
+                        duration_scale : float,
+                        return_inter_times : bool,
+                        random_seed : Union[int, None] = None):
+        t_min = self.arrival_times.min()
+        t_max = self.arrival_times.max()
+        t_start = np.random.uniform(t_min, t_max, size=max_num_sequences)
+        durations = np.random.exponential(scale=duration_scale, size=max_num_sequences)
+        t_end = np.minimum(t_start + durations, t_max)
+        masks = self.arrival_times[None, :] >= t_start[:, None]
+        masks = masks & (self.arrival_times[None, :] <= t_end[:, None])
+        valid_masks = masks.sum(axis=1) > 0
+        masks = masks[valid_masks, :]
+        
+        t_start = t_start[valid_masks]
+        t_end = t_end[valid_masks]
+        subsequence_arrival_times = [ self.arrival_times[masks[index, :]] for index in range(masks.shape[0]) ]
+        subsequence_features = [ self.features[masks[index, :], :] for index in range(masks.shape[0]) ]
+        subsequence_all_inter_times = [ np.diff(subsequence_arrival_times[index], prepend=t_start[index], append=t_end[index])  for index in range(len(subsequence_arrival_times))]
+        if(return_inter_times):
+            subsequence_censored_inter_times = np.array([ subsequence_all_inter_times[index][-1]   for index in range(len(subsequence_all_inter_times))])
+            subsequence_inter_times = [ subsequence_all_inter_times[index][:-1]  for index in range(len(subsequence_all_inter_times))]
+            return subsequence_inter_times, subsequence_censored_inter_times, subsequence_features, t_start, t_end
+        else:
+            return subsequence_arrival_times, subsequence_features, t_start, t_end
+    @staticmethod
+    def pack_sequences(sequences: List[np.ndarray]):
+        lengths = [ seq.shape[0] for seq in sequences ]
+        max_length = max(lengths)
+        batch_size = len(sequences)
+        feature_dim = sequences[0].shape[1] if len(sequences[0].shape) > 1 else 1
+        packed_array = np.zeros((batch_size, max_length, feature_dim))
+        mask = np.zeros((batch_size, max_length), dtype=bool)
+        for i, seq in enumerate(sequences):
+            length = seq.shape[0]
+            packed_array[i, :length, ...] = seq
+        return packed_array, np.array(lengths)
